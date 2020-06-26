71ae57e1d599448153e71eed81c3c416c892b103
steve olechowski <sjo@blinkfire.com>
2020-06-26 17:31:37 -0500

    new egg

--- setup.py
+++ setup.py
@@ -11,5 +11,17 @@ setup(name='TTBot',
         author_email='apisite@126.com',
         description="TTBot",
         url='https://github.com/steveobd/TTBot',
-        install_requires=[],
+        install_requires=[
+        'opencv-python==4.1.0.25',
+        'PyExecJS==1.5.1',
+        'pymongo==3.7.1',
+        'requests==2.20.0',
+        'selenium==3.14.1',
+        'urllib3==1.24.2',
+        'validators==0.13.0',
+        'w3lib==1.19.0',
+        'bs4'
+
+
+        ],
         )
