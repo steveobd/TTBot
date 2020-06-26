931fc7f27fdc4ef1aab5d56fd7294d52a9550eda
steve olechowski <sjo@blinkfire.com>
2020-06-26 15:23:05 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+PACKAGES = [
+    "TTBot"
+]
+
+setup(name='TTBot',
+        packages=PACKAGES,
+        version='3.0',
+        author='01ly',
+        author_email='apisite@126.com',
+        description="TTBot",
+        url='https://github.com/steveobd/TTBot',
+        install_requires=[],
+        )
